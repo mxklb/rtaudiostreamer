257881fb665ae8060d3e20d1e3ee759f66548452
mxklb <dev@xamblak.de>
2017-01-31 22:38:57 +0100

    Fixed initialization tests to only run if channels > 0

--- libs/audiostreamer/audiostreamer-test/main.cpp
+++ libs/audiostreamer/audiostreamer-test/main.cpp
@@ -7,25 +7,27 @@ TEST_CASE( "AudioStreamer", "[AudioStreamer]" ) {
         AudioStreamer streamer;
         unsigned int deviceId = 0;
 
-        // Test numberOfInputChannels
-        streamer.setActiveDevice(deviceId);
-        unsigned int numOfInputs = streamer.getListOfDevices().at(deviceId).inputChannels;
-        REQUIRE(streamer.numberOfInputChannels() == numOfInputs);
+        if( streamer.getListOfDevices().size() != 0 ) {
+            // Test numberOfInputChannels
+            streamer.setActiveDevice(deviceId);
+            unsigned int numOfInputChannels = streamer.getListOfDevices().at(deviceId).inputChannels;
+            REQUIRE(streamer.numberOfInputChannels() == numOfInputChannels);
 
-        // Test setActiveChannels (all channels 0..N-1 and reverse N-1..0)
-        QList<bool> channelSort({false, true});
-        foreach (bool reverse, channelSort) {
-            QList<unsigned int> channels;
-            for( unsigned int ch=0; ch<numOfInputs; ch++ ) {
-                if( !reverse ) channels.push_back(ch);
-                else channels.push_front(ch);
-            }
-            streamer.setActiveChannels(channels);
-            REQUIRE(streamer.getInputChannelIds().size() == numOfInputs);
+            // Test setActiveChannels (all channels 0..N-1 and reverse N-1..0)
+            QList<bool> channelSort({false, true});
+            foreach (bool reverse, channelSort) {
+                QList<unsigned int> channels;
+                for( unsigned int ch=0; ch<numOfInputChannels; ch++ ) {
+                    if( !reverse ) channels.push_back(ch);
+                    else channels.push_front(ch);
+                }
+                streamer.setActiveChannels(channels);
+                REQUIRE(streamer.getInputChannelIds().size() == numOfInputChannels);
 
-            for( unsigned int ch=0; ch<numOfInputs; ch++ ) {
-                if( !reverse ) REQUIRE(streamer.getInputChannelIds().at(ch) == channels.at(ch));
-                else REQUIRE(streamer.getInputChannelIds().at(ch) == channels.at(numOfInputs-1-ch));
+                for( unsigned int ch=0; ch<numOfInputChannels; ch++ ) {
+                    if( !reverse ) REQUIRE(streamer.getInputChannelIds().at(ch) == channels.at(ch));
+                    else REQUIRE(streamer.getInputChannelIds().at(ch) == channels.at(numOfInputChannels-1-ch));
+                }
             }
         }
     }
