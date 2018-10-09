7fdcbeeadf06e3765acf4dc03dcd4023703ea634
mxklb <dev@xamblak.de>
2018-10-09 22:51:03 +0200

    Allways add .appveyor.yml to branch osx-deploy #10

--- pkgs/osx/deploy.sh
+++ pkgs/osx/deploy.sh
@@ -12,6 +12,7 @@ cd $scriptPath
 
 mkdir dmg
 cp $scriptPath/../../app/RtAudioStreamer-*.dmg dmg/RtAudioStreamer.dmg
+cp $scriptPath/appveyor.yml dmg/.appveyor.yml
 cp $scriptPath/travis.yml dmg/.travis.yml
 cp $scriptPath/README.md dmg/README.md
 cd dmg
